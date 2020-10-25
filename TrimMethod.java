2432264e7f9cf92ebd87c0a975e9b5c1a06885f3
Shaik Faizan Roshan Ali <67701441+faizanroshan@users.noreply.github.com>
2020-10-25 23:58:10 +0530

    Update TrimMethod.java

--- TrimMethod.java
+++ TrimMethod.java
@@ -1,19 +1,12 @@
-/*
-	Author : Shaik Faizan Roshan Ali
-	Email : alsahercoder@gmail.com
-	Date : 17-oct-2020
-	An self implementation of trim method of Java.	
-*/
-
 import java.util.*;
 
 class TrimMethod {
 
     public static void main(String args[]) {
 
-        String testStr1 = "  Hello world    ";  // Leading and trailing spaces
-        String testStr2 = "     Hello World";  // Tabspace 
-        String testStr3 = "Hello World";      // No leading or trailing spaces
+        String testStr1 = "  Hello world    "; // leading and trailing spaces
+        String testStr2 = "     Hello World";  // tabspace 
+        String testStr3 = "Hello World";    //No leading or trailing spaces
 
         String trimTest1 = trim(testStr1);
         String trimTest2 = trim(testStr2);
@@ -27,42 +20,36 @@ class TrimMethod {
     static String trim(String str) {
 
         char[] charArray = str.toCharArray();
-        boolean flag = false;
-        String trimmedString = "";
+        int leadingIndex = 0;
+        int trailingIndex = str.length();
+
         for( int index = 0; index < charArray.length; index++ ) {
 
-            if( !(Character.isWhitespace(charArray[index])) || flag == true ) {
+            if( !(Character.isWhitespace(charArray[index])) ) {
 
-                trimmedString = trimmedString.concat(String.valueOf(charArray[index]));
-                flag = true;
+                leadingIndex = index;
+                break;
             }
 
         }
 
-        flag = false;
-        char[] charArray1 = trimmedString.toCharArray();
-        int stringLength = trimmedString.length();
-        trimmedString = "";
+        // 
+        for( int index = charArray.length - 1; index >= 0; index-- ) {
 
-        // This next Loop prints the mirror output after removing leading and trailing spaces.
-        for( int index = stringLength - 1; index >= 0; index-- ) {
+            if( !(Character.isWhitespace(charArray[index])) ) {
 
-            if( !(Character.isWhitespace(charArray1[index])) || flag == true ) {
-
-                trimmedString = trimmedString.concat(String.valueOf(charArray1[index]));
-                flag = true;
+                trailingIndex = index;
+                break;
             }
 
         }
 
-        stringLength = trimmedString.length();
-        String outputString = "";
+        char[] outputArray  = new char [charArray.length];
+        int inputIndex = 0;
+        for ( int index = leadingIndex; index <= trailingIndex; index++ ) {
 
-        for ( int index = stringLength - 1; index >=0; index-- ) {
-
-            outputString = outputString.concat(String.valueOf(trimmedString.charAt(index)));
+            outputArray[inputIndex] = charArray[index];
+            inputIndex++;
         }
-        
-        return outputString;
-    }
-}
+
+
